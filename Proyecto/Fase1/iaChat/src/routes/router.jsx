09ed23c7c0cc0b172406f9a93de347f7bddda06c
Iskandar1412 <silvapaco810@gmail.com>
2024-12-09 13:09:45 -0600

    [FIX]: Update routers

--- Proyecto/Fase1/iaChat/src/routes/router.jsx
+++ Proyecto/Fase1/iaChat/src/routes/router.jsx
@@ -1,21 +1,21 @@
-import { createBrowserRouter } from "react-router-dom";
-import { Chat } from "../pages/Chat";
-import { Layout } from "../containers/chat/Layout";
-import { ChatbotProvider } from "../context/ChatboxContext";
+// import { createBrowserRouter } from "react-router-dom";
+// import { Chat } from "../pages/Chat";
+// import { Layout } from "../containers/chat/Layout";
+// import { ChatbotProvider } from "../context/ChatboxContext";
 
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <ChatbotProvider>
-        <Layout>
-          <Chat />
-        </Layout>
-      </ChatbotProvider>
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: (
+//       <ChatbotProvider>
+//         <Layout>
+//           <Chat />
+//         </Layout>
+//       </ChatbotProvider>
   
-    ),
-  },
-]);
+//     ),
+//   },
+// ]);
 
-export default router;
+// export default router;
